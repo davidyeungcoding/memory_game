acb6f3ba980b9504f732fb9bba62a52473505b34
davidyeungcoding <yeungpirate@gmail.com>
2020-09-17 15:13:25 -0700

    Added footer and game instructions. Minor changes to css.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GameBoardComponent } from './game-board/game-board.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameBoardComponent,
     NavigationComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
